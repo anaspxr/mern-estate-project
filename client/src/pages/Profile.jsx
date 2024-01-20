1065c3df21748325e5d0f45804c6113d1e490963
ANAS <anaspappadan@gmail.com>
2024-01-20 15:05:48 +0530

    update listing api routes and functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -167,7 +167,7 @@ export default function Profile() {
   const handleListingDelete = async (listingId) => {
     try {
       const res = await fetch(`/api/listing/delete/${listingId}`, {
-        method: 'DELETE',
+        method: "DELETE",
       });
       const data = await res.json();
       if (data.success === false) {
@@ -361,13 +361,15 @@ export default function Profile() {
               </Link>
 
               <div className="flex flex-col item-center">
-              <button
+                <button
                   onClick={() => handleListingDelete(listing._id)}
-                  className='text-red-700 uppercase'
+                  className="text-red-700 uppercase"
                 >
                   Delete
                 </button>
-                <button className="text-green-700 uppercase">Edit</button>
+                <Link to={`/update-listing/${listing._id}`}>
+                  <button className="text-green-700 uppercase">Edit</button>
+                </Link>
               </div>
             </div>
           ))}
